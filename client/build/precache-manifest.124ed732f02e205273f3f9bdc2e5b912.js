f2d36eef030bfc43f4b5d2c84f89de57ba2f97a4
Shannon Silver <lineshan@msu.edu>
2019-04-29 20:12:53 -0700

    commit

---
+++ client/build/precache-manifest.124ed732f02e205273f3f9bdc2e5b912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa73a610deaf0c02fd19",
+    "url": "/static/css/main.9ae2e5f1.chunk.css"
+  },
+  {
+    "revision": "fa73a610deaf0c02fd19",
+    "url": "/static/js/main.9806384a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26553e7036fb655a5e00",
+    "url": "/static/js/2.f43e1651.chunk.js"
+  },
+  {
+    "revision": "831d1d49741d4fb969a57611a05eb6b0",
+    "url": "/index.html"
+  }
+];
